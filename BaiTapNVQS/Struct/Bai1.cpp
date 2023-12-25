24cbf126abc14a9ca7a87a70bb63ee45a1312b25
Hoang Le <hoang.le2160.4work@gmail.com>
2023-12-25 10:46:04 +0700

    updated

--- BaiTapNVQS/Struct/Bai1.cpp
+++ BaiTapNVQS/Struct/Bai1.cpp
@@ -1,27 +1,50 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
 
-struct SinhVien
+struct DonThuc
 {
-    string ten;
-    int mssv;
-    int gpa;
-    string diachi;
+    float He_so;
+    int Bac;
+
+    void Nhap()
+    {
+        cout << "Nhap he so: ";
+        cin >> He_so;
+        cout << "Nhap bac cua he so: ";
+        cin >> Bac;
+    }
+
+    void Xuat()
+    {
+        cout << He_so << "x^" << Bac;
+    }
+
+    // Operator overload
+    DonThuc operator+(const DonThuc other)
+    {
+        DonThuc Tong;
+        if (Bac == other.Bac)
+        {
+            Tong.He_so = He_so + other.He_so;
+            Tong.Bac = Bac;
+            return Tong;
+        }
+        else
+        {
+            cout << "ERROR!\0";
+            Tong.He_so = 0.0;
+            Tong.Bac = 0;
+        }
+        return Tong;
+    }
 };
 
 int main()
 {
-
-    SinhVien Phuoc;
-    cin.ignore();
-    getline(cin, Phuoc.ten);
-    cin >> Phuoc.mssv;
-    cin >> Phuoc.gpa;
-    cin.ignore();
-    getline(cin, Phuoc.diachi);
-
-    cout
-        << Phuoc.ten << " " << Phuoc.mssv << " " << Phuoc.gpa << " " << Phuoc.diachi;
+    DonThuc dt1, dt2, dt3;
+    dt1.Nhap();
+    dt2.Nhap();
+    dt3 = dt1 + dt2;
+    dt3.Xuat();
     return 0;
 }
